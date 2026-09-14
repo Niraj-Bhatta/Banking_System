bf57d686cd0e9cfaf4790f7100e3a832088899b1
Niraj Bhatta <bhattaniraj559@gmail.com>
2026-09-14 13:09:44 +0545

    fixed the ui of the project

--- bank_system/banking/signals.py
+++ bank_system/banking/signals.py
@@ -1,30 +1,39 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from django.dispatch import receiver
-from .models import Profile, BankAccount
 from django.contrib.auth.signals import user_logged_in, user_logged_out
-from .models import UserActivity
+from django.dispatch import receiver
+
+from .models import Profile, BankAccount, UserActivity
+
 
 @receiver(post_save, sender=User)
 def create_profile_and_account(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
-        BankAccount.objects.create(user=instance)
+        Profile.objects.get_or_create(user=instance)
+        BankAccount.objects.get_or_create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_profile_and_account(sender, instance, **kwargs):
-    instance.profile.save()
-    instance.bank_account.save()
+    profile, _ = Profile.objects.get_or_create(user=instance)
+    bank_account, _ = BankAccount.objects.get_or_create(user=instance)
 
+    profile.save()
+    bank_account.save()
 
-from django.contrib.auth.signals import user_logged_in, user_logged_out
-from django.dispatch import receiver
-from .models import UserActivity
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
-    UserActivity.objects.create(user=user, activity_type='Login')
+    UserActivity.objects.create(
+        user=user,
+        activity_type='Login'
+    )
+
 
 @receiver(user_logged_out)
 def log_user_logout(sender, request, user, **kwargs):
-    UserActivity.objects.create(user=user, activity_type='Logout')
+    UserActivity.objects.create(
+        user=user,
+        activity_type='Logout'
+    )
+
